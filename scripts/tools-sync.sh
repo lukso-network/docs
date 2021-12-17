0937a266c96c168ea391dcb5bd1ebebeeea74574
Callum Grindle <54543428+CallumGrindle@users.noreply.github.com>
2021-12-17 14:17:03 +0000

    chore: refactor tools technical reference  (#68)
    * chore: link to external tools technical reference docs
    
    * chore: sync docs
    
    * chore: update erc725.js technical reference docs link

--- scripts/tools-sync.sh
+++ scripts/tools-sync.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-SEDOPTION="-i"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  SEDOPTION="-i ''"
-fi
-
 # Pull erc725.js repo
 mkdir tmpDocsSync 
 cd tmpDocsSync
@@ -13,17 +8,6 @@ rm erc725.js/docs/README.md
 
 # Copy Docs
 rsync -av --progress erc725.js/docs/. ../docs/tools/erc725js --exclude technical-reference
-cp erc725.js/docs/technical-reference/classes/ERC725.md ../docs/tools/erc725js/technical-reference/ERC725.md
-
-# Update links in Technical Ref Docs spec
-sed $SEDOPTION 's,(\.\.,(https://github.com/ERC725Alliance/erc725.js/main/develop/docs/technical-reference,g' ../docs/tools/erc725js/technical-reference/ERC725.md
-sed $SEDOPTION 1d ../docs/tools/erc725js/technical-reference/ERC725.md
-
-echo '---
-sidebar_label: Methods
----
-
-# Class: ERC725'|cat - ../docs/tools/erc725js/technical-reference/ERC725.md > /tmp/out && mv /tmp/out ../docs/tools/erc725js/technical-reference/ERC725.md
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -33,44 +17,6 @@ git clone --depth 1 --branch main https://github.com/lukso-network/tools-lsp-fac
 
 # Copy Docs
 rsync -av --progress tools-lsp-factory/docs/. ../docs/tools/lsp-factoryjs --exclude=technical-reference
-
-# make technical-reference/methods docs
-mkdir ../docs/tools/lsp-factoryjs/technical-reference && touch methods.md
-echo '---
-sidebar_label: Methods
-sidebar_position: 1.1
----
-' > ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-
-#  Add LSPFactory Spec
-echo '
-## Class: LSPFactory
-' >> ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-sed $SEDOPTION 's,(\.\.,(https://github.com/lukso-network/tools-lsp-factory/tree/main/docs/technical-reference,g' tools-lsp-factory/docs/technical-reference/classes/LSPFactory.md
-sed $SEDOPTION /\#\#/s/^/\#/ tools-lsp-factory/docs/technical-reference/classes/LSPFactory.md
-sed $SEDOPTION '/#\#\#\# Constructors/d' tools-lsp-factory/docs/technical-reference/classes/LSPFactory.md
-awk '/### Constructors/{flag=1} /## Properties/{flag=0} flag' tools-lsp-factory/docs/technical-reference/classes/LSPFactory.md >> ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-
-#  Add LSP3UniversalProfile Spec
-echo '
-## Class: LSP3UniversalProfile
-' >> ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-sed $SEDOPTION 's,(\.\.,(https://github.com/lukso-network/tools-lsp-factory/tree/main/docs/technical-reference,g' tools-lsp-factory/docs/technical-reference/classes/LSP3UniversalProfile.md
-sed $SEDOPTION /\#\#/s/^/\#/ tools-lsp-factory/docs/technical-reference/classes/LSP3UniversalProfile.md
-sed $SEDOPTION '/#\#\#\# Constructors/d' tools-lsp-factory/docs/technical-reference/classes/LSP3UniversalProfile.md
-awk '/### Constructors/{flag=1} /asd/{flag=0} flag' tools-lsp-factory/docs/technical-reference/classes/LSP3UniversalProfile.md >> ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-
-#  Add DigitalAsset Spec
-echo '
-## Class: DigitalAsset
-' >> ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-sed $SEDOPTION 's,(\.\.,(https://github.com/lukso-network/tools-lsp-factory/tree/main/docs/technical-reference,g' tools-lsp-factory/docs/technical-reference/classes/DigitalAsset.md
-sed $SEDOPTION /\#\#/s/^/\#/ tools-lsp-factory/docs/technical-reference/classes/DigitalAsset.md
-sed $SEDOPTION '/#\#\#\# Constructors/d' tools-lsp-factory/docs/technical-reference/classes/DigitalAsset.md
-awk '/### Constructors/{flag=1} /asd/{flag=0} flag' tools-lsp-factory/docs/technical-reference/classes/DigitalAsset.md >> ../docs/tools/lsp-factoryjs/technical-reference/methods.md
-
-# Embedded links break inside chevrons so add a space
-sed $SEDOPTION 's,\\>, \\>,g' ../docs/tools/lsp-factoryjs/technical-reference/methods.md
 
 cd ..
 rm -rf tmpDocsSync
