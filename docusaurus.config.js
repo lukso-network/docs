012943710eb20b5920b1ba0ca5b93c37850770a8
Hugo Masclet <hugo@lukso.io>
2022-01-06 16:01:09 +0100

    Add redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,6 +133,41 @@ module.exports = {
         },
       },
     ],
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/contracts/overview',
+            to: '/standards/smart-contracts/overview',
+          },
+          {
+            from: '/contracts/erc725-account',
+            to: '/standards/smart-contracts/lsp0-erc725-account',
+          },
+          {
+            from: '/contracts/universal-receiver-delegate',
+            to: '/standards/smart-contracts/lsp1-universal-receiver-delegate',
+          },
+          {
+            from: '/contracts/key-manager',
+            to: '/standards/smart-contracts/lsp6-key-manager',
+          },
+          {
+            from: '/contracts/digital-asset',
+            to: '/standards/smart-contracts/lsp7-digital-asset',
+          },
+          {
+            from: '/contracts/identifiable-digital-asset',
+            to: '/standards/smart-contracts/lsp8-identifiable-digital-asset',
+          },
+          {
+            from: '/contracts/interface-ids',
+            to: '/standards/smart-contracts/interface-ids',
+          },
+        ],
+      },
+    ],
   ],
   themes: ['@saucelabs/theme-github-codeblock'],
 };
