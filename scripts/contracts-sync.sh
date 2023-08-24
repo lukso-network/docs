dde048578dfb0946e8ced566deaee9e896612d22
CJ42 <cavallerajean@gmail.com>
2023-08-24 17:23:16 +0100

    ci: move syncing of smart contracts docs to `develop` branch

--- scripts/contracts-sync.sh
+++ scripts/contracts-sync.sh
@@ -3,7 +3,7 @@
 # Pull lsp-smart-contracts repo
 mkdir tmpDocsSync 
 cd tmpDocsSync
-git clone --depth 1  --branch test-docs-sync https://github.com/lukso-network/lsp-smart-contracts.git
+git clone --depth 1  --branch develop https://github.com/lukso-network/lsp-smart-contracts.git
 
 # Copy Docs
 rsync -av --progress lsp-smart-contracts/docs/. ../docs/contracts/
