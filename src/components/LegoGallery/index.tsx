f64833d7918afb6ea894b4622eb75a4eb6aa7a45
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-01-04 11:24:16 +0100

    trip desc and fix card text styles

--- src/components/LegoGallery/index.tsx
+++ src/components/LegoGallery/index.tsx
@@ -6,120 +6,136 @@ import clsx from 'clsx';
 
 const STANDARDS = [
   {
-    title: 'LSP0 - ERC725 Account',
+    title: 'LSP0',
+    name: 'ERC725 Account',
     link: './universal-profile/lsp0-erc725account',
     lightImage: '/img/standards/legos/LSP0-lego.png',
     darkImage: '/img/standards/legos/LSP0-lego.png',
-    text: 'A blockchain based account that can own assets and be used by people, organizations, etc...',
+    text: 'Blockchain account for asset ownership and use by individuals or entities.',
   },
   {
-    title: 'LSP1 - Universal Receiver',
+    title: 'LSP1',
+    name: 'Universal Receiver',
     link: './generic-standards/lsp1-universal-receiver',
     lightImage: '/img/standards/legos/LSP1-lego.png',
     darkImage: '/img/standards/legos/LSP1-lego.png',
-    text: 'Allows to receive notifications about incoming and outgoing transactions or informations.',
+    text: 'Notification receiver for incoming and outgoing transactions or information.',
   },
   {
-    title: 'LSP2 - ERC725Y JSON Schema',
+    title: 'LSP2',
+    name: 'ERC725Y JSON Schema',
     link: './generic-standards/lsp2-json-schema',
     lightImage: '/img/standards/legos/LSP2-lego.png',
     darkImage: '/img/standards/legos/LSP2-lego.png',
-    text: 'A schema to describe how ERC725Y data key-value pairs can be interpreted and encoded/decoded.',
+    text: 'Describes ERC725Y data key-value pairs interpretation including encoding and decoding.',
   },
   {
-    title: 'LSP3 - Profile Metadata',
+    title: 'LSP3',
+    name: 'Profile Metadata',
     link: './universal-profile/lsp3-profile-metadata',
     lightImage: '/img/standards/legos/LSP3-lego.png',
     darkImage: '/img/standards/legos/LSP3-lego.png',
-    text: 'A set of ERC725Y data keys useful to describe a smart contract based profile.',
+    text: 'Set of ERC725Y data keys to describe a smart contract based blockchain profile.',
   },
   {
-    title: 'LSP4 - Digital Asset Metadata',
+    title: 'LSP4',
+    name: 'Digital Asset Metadata',
     link: './tokens/LSP4-Digital-Asset-Metadata',
     lightImage: '/img/standards/legos/LSP4-lego.png',
     darkImage: '/img/standards/legos/LSP4-lego.png',
-    text: 'A set of ERC725Y data keys useful to describe the metadata of a digital asset.',
+    text: 'Describes metadata of digital assets via a set of ERC725Y data keys.',
   },
   {
-    title: 'LSP5 - Received Assets',
+    title: 'LSP5',
+    name: 'Received Assets',
     link: './universal-profile/lsp5-received-assets',
     lightImage: '/img/standards/legos/LSP5-lego.png',
     darkImage: '/img/standards/legos/LSP5-lego.png',
-    text: 'Defines a set of ERC725Y data keys to store the addresses of assets received.',
+    text: 'Defines ERC725Y data keys storing addresses of received assets.',
   },
   {
-    title: 'LSP6 - Key Manager',
+    title: 'LSP6',
+    name: 'Key Manager',
     link: './universal-profile/lsp6-key-manager',
     lightImage: '/img/standards/legos/LSP6-lego.png',
     darkImage: '/img/standards/legos/LSP6-lego.png',
-    text: 'A smart contract and a set of predefined permissions to control an account or any smart contract through multiple addresses.',
+    text: 'Contract with permissions controlled through multiple addresses.',
   },
   {
-    title: 'LSP7 - Digital Asset',
+    title: 'LSP7',
+    name: 'Digital Asset',
     link: './tokens/LSP7-Digital-Asset',
     lightImage: '/img/standards/legos/LSP7-lego.png',
     darkImage: '/img/standards/legos/LSP7-lego.png',
-    text: 'A standard interface for digital assets that can be either fungible or non-fungible tokens.',
+    text: 'Standard for fungible or non-fungible digital assets with a unified interface.',
   },
   {
-    title: 'LSP8 - Identifiable Digital Asset',
+    title: 'LSP8',
+    name: 'Identifiable Digital Asset',
     link: './tokens/LSP8-Identifiable-Digital-Asset',
     lightImage: '/img/standards/legos/LSP8-lego.png',
     darkImage: '/img/standards/legos/LSP8-lego.png',
-    text: 'A standard interface to represent identifiable digital assets, allowing tokens and NFTs to be uniquely traded and given specific metadata.',
+    text: 'Interface for uniquely identifiable digital assets with specific metadata.',
   },
   {
-    title: 'LSP9 - Vault',
+    title: 'LSP9',
+    name: 'Vault',
     link: './universal-profile/lsp9-vault',
     lightImage: '/img/standards/legos/LSP9-lego.png',
     darkImage: '/img/standards/legos/LSP9-lego.png',
-    text: 'A version of an ERC725 smart contract that represents a blockchain vault.',
+    text: 'ERC725 smart contract variant representing a blockchain vault.',
   },
   {
-    title: 'LSP10 - Received Vaults',
+    title: 'LSP10',
+    name: 'Received Vaults',
     link: './universal-profile/lsp10-received-vaults',
     lightImage: '/img/standards/legos/LSP10-lego.png',
     darkImage: '/img/standards/legos/LSP10-lego.png',
-    text: 'A set of ERC725Y data keys to store the list of owned LSP9 Vaults.',
+    text: 'ERC725Y keys to list owned LSP9 Vaults.',
   },
   {
-    title: 'LSP12 - Issued Assets',
+    title: 'LSP12',
+    name: 'Issued Assets',
     link: './universal-profile/lsp12-issued-assets',
     lightImage: '/img/standards/legos/LSP12-lego.png',
     darkImage: '/img/standards/legos/LSP12-lego.png',
-    text: 'A set of ERC725Y data keys to store the list of assets created or issued by a person, entity, organization, etc...',
+    text: 'List of issued assets by an individual or entity via ERC725Y keys.',
   },
   {
-    title: 'LSP14 - Ownable 2 Steps',
+    title: 'LSP14',
+    name: 'Ownable 2 Steps',
     link: './generic-standards/lsp14-ownable-2-step',
     lightImage: '/img/standards/legos/LSP14-lego.png',
     darkImage: '/img/standards/legos/LSP14-lego.png',
-    text: 'A module to allow a safer and more secure ownership management of a smart contract.',
+    text: 'Module for secure two-step ownership management of smart contracts.',
   },
   {
-    title: 'LSP17 - Contract Extension',
+    title: 'LSP17',
+    name: 'Contract Extension',
     link: './generic-standards/lsp17-contract-extension',
     lightImage: '/img/standards/legos/LSP17-lego.png',
     darkImage: '/img/standards/legos/LSP17-lego.png',
-    text: 'A module to extend functionalities of a smart contract by using other smart contract as "extension plugins", that can be added or removed overtime.',
+    text: 'Extends smart contract functionalities with addable and removable plugins.',
   },
   {
-    title: 'LSP20 - Call Verification',
+    title: 'LSP20',
+    name: 'Call Verification',
     link: './generic-standards/lsp20-call-verification',
     lightImage: '/img/standards/legos/LSP20-lego.png',
     darkImage: '/img/standards/legos/LSP20-lego.png',
-    text: 'A module to make interactions with smart contracts easier, without the need to resolve and go through their `owner()` first.',
+    text: 'Facilitates smart contract interactions without resolving the owner first.',
   },
   {
-    title: 'LSP23 - Linked Contracts Factory',
+    title: 'LSP23',
+    name: 'Linked Contracts Factory',
     link: './generic-standards/lsp23-linked-contracts-factory',
     lightImage: '/img/standards/legos/LSP23-lego.png',
     darkImage: '/img/standards/legos/LSP23-lego.png',
-    text: 'Standard that define a factory pattern to deploy 2 contracts linked together at the pre-determined addresses, so that they can be redeployed across multiple chains.',
+    text: 'Factory pattern standard for deploying two linked contracts across chains.',
   },
 ];
 
-function LegoCard({ link, title, text, lightImage, darkImage }) {
+function LegoCard({ link, title, name, text, lightImage, darkImage }) {
   return (
     <div className={`col col--3 margin-bottom--lg ${styles.legoCard}`}>
       <Link
@@ -134,12 +150,15 @@ function LegoCard({ link, title, text, lightImage, darkImage }) {
             dark: darkImage,
           }}
           alt={title}
-          className="mt-1 w-full transition-transform group-hover:scale-110"
+          className="mt-1 w-full"
         />
-        <div className="">
-          <h3 className="mb-1.5 flex items-center gap-3 font-jakarta group-hover:text-primary">
-            <div>{title}</div>
+        <div>
+          <h3 className="flex items-center font-jakarta">
+            <div>{title}:</div>
           </h3>
+          <h4 className="flex items-center font-jakarta">
+            <div className={styles.standardName}>{name}</div>
+          </h4>
           <p className="mb-0 text-sm text-zinc-400">{text}</p>
         </div>
       </Link>
