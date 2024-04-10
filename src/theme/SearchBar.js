e0dec74b115565e9dd310270a3171d5f3e94c467
Tyler Sehr <tysehr37@gmail.com>
2024-04-10 16:27:15 -0700

    run prettier

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -6,9 +6,7 @@ export default function SearchBarWrapper(props) {
   return (
     <>
       <SearchBar {...props} />
-      <AskCookbook
-        apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjEwM2M3NTliMTZhM2M0YzE3YzU0MDEiLCJpYXQiOjE3MTIzNDAwODUsImV4cCI6MjAyNzkxNjA4NX0.4TtEbis5Pj2d5QQLnbQFeZiiQ63JBWZu2v35gDGAO94"
-      />
+      <AskCookbook apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjEwM2M3NTliMTZhM2M0YzE3YzU0MDEiLCJpYXQiOjE3MTIzNDAwODUsImV4cCI6MjAyNzkxNjA4NX0.4TtEbis5Pj2d5QQLnbQFeZiiQ63JBWZu2v35gDGAO94" />
     </>
   );
 }
