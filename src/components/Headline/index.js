6ccb044aa456719fadc78b12c62a76c589b2330b
Hugo Masclet <hugo@lukso.io>
2024-06-07 10:04:15 +0200

    Add install extension link

--- src/components/Headline/index.js
+++ src/components/Headline/index.js
@@ -4,7 +4,9 @@ import styles from './Headline.module.scss';
 export default function Headline() {
   return (
     <div className={`${styles.headline} headline`}>
-      <h1 style={{ fontWeight: 'normal', lineHeight: '1.1rem' }}>Welcome to</h1>
+      <h1 style={{ fontWeight: 'normal', lineHeight: '1.1rem' }}>
+        Welcome to the
+      </h1>
       <h1 style={{ fontWeight: 'bold' }}>LUKSO Dev Docs</h1>
     </div>
   );
