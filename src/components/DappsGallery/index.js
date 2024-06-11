16b700749ad229c46913fc3d6010b49e5f1a51f8
mustafademiray <mustafademiray@protonmail.com>
2024-06-11 17:45:41 +0300

    css tweaks, naming things better

--- src/components/DappsGallery/index.js
+++ src/components/DappsGallery/index.js
@@ -6,36 +6,44 @@ import Stakingverse from '../../../static/img/tools/stakingverse_logo.jpeg';
 import UniversalPage from '../../../static/img/tools/universalpage_logo.png';
 import Platties from '../../../static/img/tools/stakingverse_logo.jpeg';
 
-const dappsContent = [
+const dapps = [
   {
     name: 'Universal Swap',
     imageUrl: UniversalSwap,
     link: 'https://universalswaps.io/',
+    backgroundColor: '#5049df',
   },
   {
     name: 'Stakingverse',
     imageUrl: Stakingverse,
     link: 'https://stakingverse.io/',
+    backgroundColor: '#142d4b',
   },
   {
     name: 'Universal Page',
     imageUrl: UniversalPage,
     link: 'https://universal.page/',
+    backgroundColor: '#5049df',
   },
   {
     name: 'Platties',
     imageUrl: Platties,
     link: 'https://twitter.com/Platties_',
+    backgroundColor: '#142d4b',
   },
 ];
 
 export default function DappsGallery() {
   return (
-    <div className={`${styles.dappsContainer}`}>
-      {dappsContent.map((p) => (
-        <div key={p.name} className={`${styles.dappCard}`}>
-          <a href={p.link} target="_blank" rel="noreferer noopener">
-            <img src={p.imageUrl} className={`${styles.dappCardImage}`} />
+    <div className={styles.dappsContainer}>
+      {dapps.map((dapp) => (
+        <div
+          key={dapp.name}
+          style={{ backgroundColor: dapp.backgroundColor }}
+          className={styles.dappCard}
+        >
+          <a href={dapp.link} target="_blank" rel="noreferer noopener">
+            <img src={dapp.imageUrl} className={styles.dappCardImage} />
           </a>
         </div>
       ))}
