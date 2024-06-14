2661cbd18c7fead62431a7ee9ab273960ec3fdeb
mustafademiray <mustafademiray@protonmail.com>
2024-06-14 16:54:03 +0300

    added title + separator

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import PartnersIcon from '../../static/img/icons/icon-partners-gradient.png';
 import GuidesIcon from '../../static/img/icons/icon-guides-gradient.png';
 import FAQIcon from '../../static/img/icons/icon-faq-gradient.png';
 import DappsGallery from '../components/DappsGallery';
+import SeparatorWithTitle from '../components/SeperatorWithTitle';
 
 function Index() {
   return (
@@ -74,6 +75,7 @@ function Index() {
             content="Check out LUKSO ecosystem tools and services."
           />
         </div>
+        <SeparatorWithTitle title={'Projects building on LUKSO'} />
         <DappsGallery />
       </div>
     </Layout>
